e263cc935a6c44c4db5148a5b26439e1d89b36d9
elementalcharge <german.boso@gmail.com>
2017-03-01 22:25:33 -0300

    Add files via upload

---
+++ source/assets/clase2/scripts/RotacionContinua.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotacionContinua : MonoBehaviour {
+
+	public Vector3 vectorDeRotacion;//en que axis quiero que gire
+
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (vector, angulo);
+		transform.Rotate (vectorDeRotacion.x*Time.deltaTime, vectorDeRotacion.y*Time.deltaTime, vectorDeRotacion.z*Time.deltaTime);
+
+	}
+}
